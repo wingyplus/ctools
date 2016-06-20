6eaf2c374a03084a9783ace9f80a85e8a3a69352
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-06-20 11:19:07 +0700

    luajit: Add luajit Dockerfile

---
+++ luajit/hello.c
@@ -0,0 +1,10 @@
+#ifdef _cplusplus
+extern "C"
+#endif
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+const char luaJIT_BC_hello[] = {
+27,76,74,1,2,21,0,0,1,0,1,0,2,37,0,0,0,72,0,2,0,10,72,101,108,108,111,20,3,0,
+1,0,1,0,3,49,0,0,0,48,0,0,128,72,0,2,0,0,0
+};
---
+++ luajit/hello.lua
@@ -0,0 +1,5 @@
+local function hello()
+  return "Hello"
+end
+
+return hello
